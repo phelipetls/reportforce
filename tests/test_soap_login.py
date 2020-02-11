c879d40b51e89b8e01dd500ee1514e1b7843af94
phelipe <phelipe_teles@hotmail.com>
2020-02-11 19:47:32 -0300

    testing call to soap_login

--- tests/test_soap_login.py
+++ tests/test_soap_login.py
@@ -10,6 +10,27 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from reportforce.login import soap_login, AuthenticationError  # noqa: E402
 
 
+expected_url = "https://login.salesforce.com/services/Soap/u/47.0"
+
+expected_headers = {
+    "Content-Type": "text/xml; charset=UTF-8",
+    "SoapAction": "login",
+}
+
+expected_body = """<?xml version="1.0" encoding="utf-8" ?>
+    <env:Envelope
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+        xmlns:env="http://schemas.xmlsoap.org/soap/envelope/"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <env:Body>
+            <n1:login xmlns:n1="urn:partner.soap.sforce.com">
+                <n1:username>fake@username.com</n1:username>
+                <n1:password>passXXX</n1:password>
+            </n1:login>
+        </env:Body>
+    </env:Envelope>"""
+
+
 def get_login(login_type):
     xml_path = Path(__file__).resolve().parent / "sample_xml" / login_type
     with open(xml_path, "r") as xml_file:
@@ -25,6 +46,10 @@ class TestSoapLogin(unittest.TestCase):
         expected = ("sessionId", "dummy.salesforce.com")
 
         self.assertEqual(test, expected)
+
+        mockpost.assert_called_with(
+            expected_url, headers=expected_headers, data=expected_body
+        )
 
     @patch("requests.post")
     def test_failed_login(self, mockpost):
