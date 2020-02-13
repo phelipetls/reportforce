6c8d59866e44f295243b4d09edb7bb7ad18d0123
phelipe <phelipe_teles@hotmail.com>
2020-02-12 22:56:12 -0300

    solution to get matrix indices that kinda works

--- reportforce/helpers/parser.py
+++ reportforce/helpers/parser.py
@@ -1,3 +1,5 @@
+import itertools
+
 def get_cells(report):
     factmap = report["factMap"]
     # retrieving records
@@ -9,3 +11,20 @@ def get_cells(report):
 def get_column_labels_dict(metadata):
     columns_info = metadata["reportExtendedMetadata"]["detailColumnInfo"]
     return {info["label"]: column for column, info in columns_info.items()}
+
+def get_groups(groups):
+    L = []
+    for group in groups:
+        L.extend(itertools.product(*get_group_values(group)))
+    return L
+
+def get_group_values(groups):
+    L = [[groups["label"]]]
+    next_groups = groups["groupings"]
+    while next_groups:
+        level = []
+        for group in next_groups:
+            level.append(group["label"])
+        L.append([group["label"] for group in next_groups])
+        next_groups = group["groupings"]
+    return L
