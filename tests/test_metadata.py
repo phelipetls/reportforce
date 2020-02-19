d7ae8c33517281507cc14b270362f60bed713713
phelipe <phelipe_teles@hotmail.com>
2020-02-18 22:44:41 -0300

    clean up tests

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -9,7 +9,7 @@ from unittest.mock import patch
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from reportforce.report import get_metadata  # noqa: E402
-from utils import mocks
+from utils import mocks  # noqa: E402
 
 
 class TestGetMetadata(unittest.TestCase):
@@ -18,9 +18,9 @@ class TestGetMetadata(unittest.TestCase):
     headers = {"Authorization": "Bearer sessionId"}
 
     @patch("reportforce.report.request_report.GET")
-    def test_request_call_to_get_metadata(self, mockrequest):
+    def test_request_call_to_get_metadata(self, mock_request):
         get_metadata("ID", session=mocks.FakeLogin)
-        mockrequest.assert_called_with(self.url, headers=self.headers)
+        mock_request.assert_called_with(self.url, headers=self.headers)
 
 
 if __name__ == "__main__":
