200f9ff34565b217b7f8892f1fa09d775cc543ef
phelipe <phelipe_teles@hotmail.com>
2020-02-24 20:52:05 -0300

    filter by date when either start or end is passed

--- reportforce/report.py
+++ reportforce/report.py
@@ -75,7 +75,7 @@ def get_report(
 
     metadata = copy.deepcopy(get_metadata(report_id, session))
 
-    if start and end:
+    if start or end:
         filtering.set_period(start, end, date_column, metadata)
     if logic:
         filtering.set_logic(logic, metadata)
