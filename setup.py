211d339c359803811eaaf5e5b360bb7ea468aaee
Charles Koppelman <ckoppelman@polarisproject.org>
2020-04-23 22:22:50 -0400

    add dependencies to setup.py
    Without this, pip doesn't install the dependencies in requirements.txt

--- setup.py
+++ setup.py
@@ -13,7 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/phelipetls/reportforce",
     packages=setuptools.find_packages(),
-    install_requires=[],
+    install_requires=[
+        "requests",
+        "pandas"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
